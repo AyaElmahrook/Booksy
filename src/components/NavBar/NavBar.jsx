e32685d3ec4fd22e2b717ad2e3234ae7e9cd8f9d
ayahAlmahrouq <ayahalmahrouq@gmail.com>
2022-08-22 21:49:51 +0200

    active links

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,17 +2,13 @@
 import React from 'react'
 import { Disclosure} from '@headlessui/react'
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 const navigation = [
     { name: 'Home', to: 'home', current: true },
     { name: 'All Books', to: 'books', current: false },
     { name: 'Blog', to: 'blog', current: false },
 ]
-
-function classNames(...classes) {
-    return classes.filter(Boolean).join(' ')
-}
 
 export default function NavBar() {
     return (
@@ -39,10 +35,21 @@ export default function NavBar() {
                                         <h2 className="text-xl font-bold tracking-tight text-gray-900">Booksy</h2>
                                     </div>
                                 </div>
-                                {/* Search input */}
-                                <div className="md:flex space-x-10" id="txtSearch">
-                                    <input type="text" name="search" placeholder='Search by author, title, name' autoComplete="given-name" className="mt-1 focus:ring-indigo-500 w-100 p-3 bg-gray-200 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                                {/* Navigation bar */}
+                            <div className="hidden sm:block sm:ml-6 md:flex space-x-10 items-center justify-center py-4">
+                                <div className="flex space-x-4">
+                                    {navigation.map((item) => (
+                                        <NavLink
+                                            key={item.name}
+                                            to={item.to}
+                                            className={({ isActive }) => isActive ? "bg-indigo-900 text-white px-3 py-2 rounded-md text-sm font-medium" : "text-gray-600 hover:bg-indigo-900 hover:text-white px-3 py-2 rounded-md text-sm font-medium"}
+                                            aria-current={item.current ? 'page' : undefined}
+                                        >
+                                            {item.name}
+                                        </NavLink>
+                                    ))}
                                 </div>
+                            </div>
                                 {/* navbar left part */}
                                 <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
                                     <button
@@ -53,38 +60,17 @@ export default function NavBar() {
                                     </button>
                                 </div>
                             </div>
-                            {/* Navigation bar */}
-                            <div className="hidden sm:block sm:ml-6 md:flex space-x-10 items-center justify-center py-4">
-                                <div className="flex space-x-4">
-                                    {navigation.map((item) => (
-                                        <Link
-                                            key={item.name}
-                                            to={item.to}
-                                            className={classNames(
-                                                item.current ? 'bg-indigo-900 text-white' : 'text-gray-600 hover:bg-indigo-900 hover:text-white',
-                                                'px-3 py-2 rounded-md text-sm font-medium'
-                                            )}
-                                            aria-current={item.current ? 'page' : undefined}
-                                        >
-                                            {item.name}
-                                        </Link>
-                                    ))}
-                                </div>
-                            </div>
                         </div>
-
+                        {/* Mobile navigation menu */}
                         <Disclosure.Panel className="sm:hidden">
                             <div className="px-2 pt-2 pb-3 space-y-1">
                                 {navigation.map((item) => (
                                     <Disclosure.Button
                                         key={item.name}
-                                        className={classNames(
-                                            item.current ? 'bg-indigo-900 text-white' : 'text-gray-600 hover:bg-indigo-900 hover:text-white',
-                                            'block px-3 py-2 rounded-md text-base font-medium'
-                                        )}
+                                        className={({ isActive }) => isActive ? "bg-indigo-900 text-white px-3 py-2 rounded-md text-sm font-medium" : "text-gray-600 hover:bg-indigo-900 hover:text-white px-3 py-2 rounded-md text-sm font-medium"}
                                         aria-current={item.current ? 'page' : undefined}
                                     >
-                                        <Link to={item.to}>{item.name} </Link>
+                                        <NavLink to={item.to}>{item.name} </NavLink>
                                     </Disclosure.Button>
                                 ))}
                             </div>
